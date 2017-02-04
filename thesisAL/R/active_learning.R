e1a9b58a2704f7cd324b29e1cee20fbd5a729af8
klin1234@gmail.com <klin1234@gmail.com>
2017-02-04 15:44:48 -0500

    Documentation for the active learning function

--- thesisAL/R/active_learning.R
+++ thesisAL/R/active_learning.R
@@ -1,5 +1,15 @@
-#the missing labels in y are denoted by NA
-#this method takes x as a matrix of all the data
+#' Main active learning method
+#'
+#' The missing labels in y are denoted by NA.
+#' This method takes x as a matrix of all the data
+#'
+#' @param X the full data matrix, n x d, including all unlabeled data
+#' @param y a factor vector with 2 levels and NAs for unlabeled data
+#' @param method the method name
+#' @param ... additional parameters for the active learning method
+#'
+#' @return an index corresponding to the row of X to learn the label of next
+#' @export
 active_learning <- function(X, y, method = "lda", ...){
   stopifnot(nrow(X) == length(y), is.matrix(X), any(is.na(y)),
             is.factor(y), length(levels(y)) == 2)
